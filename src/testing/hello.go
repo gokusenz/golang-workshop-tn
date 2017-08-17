f06cbfe55f71dd74e19c03f74137b054c999a06a
Nattawut Ruangvivattanaroj <nattawut.ru@gmail.com>
2017-08-17 14:38:24 +0700

    add test sort

---
+++ src/testing/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"sort"
+)
+
+func sortF(data []int) []int {
+	sort.Ints(data)
+	return data
+}
---
+++ src/testing/hello_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCase1(t *testing.T) {
+	expected := []int{1}
+	result := sortF([]int{1})
+	if !reflect.DeepEqual(expected, result) {
+		t.Fatalf("Expected %v but got %v", expected, result)
+	}
+}
+
+func TestCase2(t *testing.T) {
+	expected := []int{1, 2}
+	result := sortF([]int{1, 2})
+	if !reflect.DeepEqual(expected, result) {
+		t.Fatalf("Expected %v but got %v", expected, result)
+	}
+}
+
+func TestCase3(t *testing.T) {
+	expected := []int{1, 2, 3, 4}
+	result := sortF([]int{4, 3, 2, 1})
+	if !reflect.DeepEqual(expected, result) {
+		t.Fatalf("Expected %v but got %v", expected, result)
+	}
+}
