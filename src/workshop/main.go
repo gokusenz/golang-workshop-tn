2f423a47695b5c321077379f20b35cea52cf35c0
Nattawut Ruangvivattanaroj <nattawut.ru@gmail.com>
2017-08-17 10:30:14 +0700

    add check type

--- src/workshop/main.go
+++ src/workshop/main.go
@@ -6,5 +6,15 @@ import (
 
 func main() {
 	message := "Hello world"
-	fmt.Printf("%s", message)
+	fmt.Printf("%s\n", message)
+
+	i := 1
+	f := 1.0
+	b := true
+	s := "String"
+
+	fmt.Printf("%T => %v\n", i, i)
+	fmt.Printf("%T => %v\n", f, f)
+	fmt.Printf("%T => %v\n", b, b)
+	fmt.Printf("%T => %v\n", s, s)
 }
